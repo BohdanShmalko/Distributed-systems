32decc4966b24e4f701dbea4e30ac97871396003
BohdanShmalko <43091830+BohdanShmalko@users.noreply.github.com>
2023-12-24 12:31:58 +0200

    small bug fix +

--- apps/api/main.js
+++ apps/api/main.js
@@ -42,7 +42,9 @@ app.get('/metrics', async (req, res) => {
         }
 
         const data = await getByPeriod(inputParams);
-        await redisClient.set(cacheKey, REDIS_TTL ? Number(REDIS_TTL) : 3600, JSON.stringify(data));
+        await redisClient.set(cacheKey, JSON.stringify(data), {
+            EX: REDIS_TTL ? Number(REDIS_TTL) : 3600
+        });
         res.status(200).send(JSON.stringify({ usedCache: false, data }));
         
     } catch (error) {
